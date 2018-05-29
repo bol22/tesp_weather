7a188f17beb04f33b493ca32ee8d1fa84bb18e81
bol22 <328005509@qq.com>
2018-05-29 16:45:09 -0700

    update

--- NOAA_test.py
+++ NOAA_test.py
@@ -3,27 +3,31 @@ Created on Mon May 28 11:02:28 2018
 
 @author: liuboming
 """
+import requests, json, csv
+
 myToken='GlQbxfsaOUPCWrtJylfRwRuXwAZJnyVK'
-myUrl='https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=TOBS&datacatogoryid=TEMP&stationid=COOP:010008&units=standard&startdate=2010-05-01&enddate=2010-05-15'
+#myUrl='https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=NORMAL_HLY&stationid=COOP:010008&units=metric&startdate=2014-05-01&enddate=2014-05-10'
+myUrl='https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?datasetsid=NORMAL_HLY'
 
 head={'token':myToken}
-import requests, json, csv
 
 r=requests.get(url=myUrl, headers=head)
 data=r.json()
 
-outputFile = open("TOBS_half_month.csv","w")
-outputWriter = csv.writer(outputFile)
-
-# obtain keys in the dict
-keys=list(data['results'][0].keys()) 
+if data=={}:
+    print('Wrong url, no data avaliable')
+else :
+    outputFile = open("location_hourly.csv","w")
+    outputWriter = csv.writer(outputFile)
+    # obtain keys in the dict
+    keys=list(data['results'][0].keys()) 
 # write keys in the first line
-outputWriter.writerow(keys)
+    outputWriter.writerow(keys)
 
-for location in data['results']:
-    row_array=[]
-    for attribute in location:
-        row_array.append(location[attribute])
-    outputWriter.writerow(row_array)
-outputFile.close()
+    for location in data['results']:
+        row_array=[]
+        for attribute in location:
+            row_array.append(location[attribute])
+        outputWriter.writerow(row_array)
+#outputFile.close()
         
