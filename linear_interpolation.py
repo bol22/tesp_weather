ce97c0151214439706db160d5da0c4c7592d9381
bol22 <328005509@qq.com>
2018-05-30 16:18:04 -0700

    linear interpolation

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -6,8 +6,18 @@ Created on Wed May 30 15:52:51 2018
 """
 
 # linear interpolate for the hourly data to make 5 minutes data.
-csvFile = open("temperature_mean_seattleairport_sim.csv", "r")
-reader = csv.reader(csvFile)
-list1=[]
-for row in reader:
-    list1.append(row)
+#csvFile = open("temperature_mean_seattleairport_sim.csv", "r")
+#reader = csv.reader(csvFile)
+#list1=[]
+#for row in reader:
+#    list1.append(row)
+
+import numpy as np
+from matplotlib.dates import strpdate2num
+
+time, temp = np.loadtxt('temperature_mean_seattleairport_sim.csv', skiprows=1,
+        converters={2:strpdate2num('%H:%M:%S')}, unpack=True)
+
+
+interp_time = np.linspace(time[0], time[-1], 100)
+interp_temp = np.interp(interp_time, time, temp)
